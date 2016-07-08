4b4bf299cce93b9817ae127c7aa0b223ee109d83
Jaan Toots <jaantoots@gmail.com>
2016-07-08 16:44:33 +0100

    Get objects with a given color

--- labels.py
+++ labels.py
@@ -1,6 +1,11 @@
 """Provides methods for labelling the model correctly for rendering"""
 import bpy
 
-def materials_with(color):
-    """Returns all materials with a given diffuse_color"""
-    return [material for material in bpy.data.materials if material.diffuse_color == color]
+def objects_with(color):
+    """Returns all objects with a specified color"""
+
+    # Materials with a given diffuse_color
+    materials = [material for material in bpy.data.materials if material.diffuse_color == color]
+
+    return [obj for obj in bpy.data.objects if obj.material_slots[0].material in materials]
+
