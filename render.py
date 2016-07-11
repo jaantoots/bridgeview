2c4c1decc6e8069485de4717162ade1186d99532
Jaan Toots <jaantoots@gmail.com>
2016-07-11 19:32:35 +0100

    Render class (refactor) added; debugged render

--- render.py
+++ render.py
@@ -1,57 +1,25 @@
 """Provides methods for rendering the labelled model"""
 import os
+import json
 import numpy as np
 import bpy # pylint: disable=import-error
 from mathutils import Vector # pylint: disable=import-error
 
+# TODO: Remove ground plane function, belongs with terrain
 def add_ground_plane():
     """Add a large ground plane"""
-    bpy.ops.mesh.primitive_plane_add(radius=1000)
+    bpy.ops.mesh.primitive_plane_add(location=(0, 0, 0), radius=1000)
     return bpy.context.object
 
-def randomise_sun(sun=None):
-    """Delete a previous sun (if given) and create a new one at a random angle"""
-    bpy.ops.object.select_all(action='DESELECT')
-    if sun is not None:
-        sun.select = True
-        bpy.ops.object.delete()
-
-    theta = np.random.uniform(0, 17/18 * np.pi/2) # Not lower than 5 deg from horizon
-    bpy.ops.object.lamp_add(type='SUN', location=(0, 0, 20),
-                            rotation=(theta, 0, np.random.uniform(0, 2*np.pi)))
-    sun = bpy.context.object
-
-    # TODO: check results and modify sun size
-    sun.data.shadow_soft_size = 0.01 # Realistic sun is smaller than the defaul
-    return sun
-
-def new_camera():
-    """Add a camera to the scene"""
+def new_camera(resolution):
+    """Add a camera to the scene and set the resolution for rendering"""
     bpy.ops.object.camera_add()
     camera = bpy.context.object
     bpy.data.scenes[0].camera = camera
-    return camera
-
-def randomise_camera(objects, camera=None):
-    """Randomise the camera position with the objects in view"""
-    camera = new_camera() if camera is None else camera
-
-    # Position and face centre
-    sphere = BoundingSphere(objects)
-    min_distance = sphere.radius / np.tan(camera.angle_y/2) # Height smaller than width
-    distance = np.random.normal(min_distance * 5/3, min_distance/2) # Arbitrary parameters here
-    theta = np.random.uniform(np.pi/6, np.pi/2) # Maybe need to limit this further
-    phi = np.random.uniform(0, 2*np.pi)
-
-    # Location axes rotated due to default camera position
-    location = sphere.centre + distance * np.array(
-        [np.sin(theta)*np.sin(-phi), np.sin(theta)*np.cos(phi), np.cos(theta)])
-    rotation = np.array([theta, 0, phi])
-    rotation += np.random.randn(3) * 0.1 # Arbitrary parameter
-
-    camera.location = location
-    camera.rotation_euler[:] = rotation
-
+    bpy.data.scenes[0].render.resolution_x = resolution[0]
+    bpy.data.scenes[0].render.resolution_y = resolution[1]
+    bpy.data.scenes[0].render.resolution_percentage = 100
+    camera.data.clip_end = 2000 # Maybe set dynamically if ground plane larger
     return camera
 
 class BoundingSphere():
@@ -69,12 +37,85 @@ class BoundingSphere():
         box = np.array([[minmax(i, j)([(x.matrix_world * Vector(x.bound_box[i]))[j]
                                        for x in objects]) for j in range(3)] for i in range(8)])
         self.centre = np.sum(box, axis=0)/8 if centre is None else centre
-        self.radius = np.max(np.linalg.norm(box - centre, axis=1))
+        self.radius = np.max(np.linalg.norm(box - self.centre, axis=1))
 
-def render(path, seq=0):
-    """Render the scene"""
-    if not os.path.exists(path):
-        os.makedirs(path)
-    bpy.data.scenes[0].render.filepath = os.path.join(path, "{:03d}.png".format(seq)) # limit 999
+class Render():
+    """Configure and render the scene"""
 
-    bpy.ops.render.render()
+    def __init__(self, objects, conf_file=None):
+        self.objects = objects
+        self.sphere = BoundingSphere(self.objects)
+        self.sun = None
+        self.camera = None
+
+        if conf_file is None:
+            self._default()
+        else:
+            with open(conf_file) as file:
+                self.opts = json.load(file)
+
+    def _default(self):
+        self.opts = {}
+        self.opts['sun_theta'] = [0, 17/18 * np.pi/2] # Not lower than 5 deg from horizon
+        self.opts['sun_size'] = 0.02 # Realistic sun is smaller than the default value
+        self.opts['sun_strength'] = 2
+        self.opts['camera_distance_factor'] = [6/12, 1/12] # [mu, sigma] = factor * min_distance
+        self.opts['camera_theta'] = [np.pi/6, 0] # Maybe restrict further. Allow views from below?
+        self.opts['camera_noise'] = 0.01 # Random rotation sigma [x, y, z] or float
+        self.opts['resolution'] = [512, 512] # [x, y] pixels
+        self.opts['cycles_samples'] = 64 # Increase to reduce noise
+
+    def write_conf(self, conf_file):
+        """Write current configuration to conf_file"""
+        with open(conf_file, 'w') as file:
+            json.dump(self.opts, file)
+
+    def randomise_sun(self):
+        """Delete a previous sun (if exists) and create a new one at a random angle"""
+        bpy.ops.object.select_all(action='DESELECT')
+        if self.sun is not None:
+            self.sun.select = True
+            bpy.ops.object.delete()
+
+        theta = np.random.uniform(self.opts['sun_theta'][0], self.opts['sun_theta'][1])
+        bpy.ops.object.lamp_add(type='SUN', location=(0, 0, 20),
+                                rotation=(theta, 0, np.random.uniform(0, 2*np.pi)))
+        self.sun = bpy.context.object
+
+        # Set size and strength
+        self.sun.data.shadow_soft_size = self.opts['sun_size']
+        self.sun.data.node_tree.nodes['Emission'].inputs['Strength'].default_value \
+            = self.opts['sun_strength']
+        return self.sun
+
+    def randomise_camera(self):
+        """Randomise the camera position with the objects in view"""
+        if self.camera is None:
+            self.camera = new_camera(self.opts['resolution'])
+
+        # Position and face centre
+        min_distance = self.sphere.radius / np.tan(self.camera.data.angle_y/2) # Height < width
+        distance = np.random.normal(min_distance * self.opts['camera_distance_factor'][0],
+                                    min_distance * self.opts['camera_distance_factor'][1])
+        theta = np.random.uniform(self.opts['camera_theta'][0], self.opts['camera_theta'][1])
+        phi = np.random.uniform(0, 2*np.pi)
+
+        # Location axes rotated due to default camera position
+        location = self.sphere.centre + distance * np.array(
+            [np.sin(theta)*np.sin(-phi), np.sin(theta)*np.cos(phi), np.cos(theta)])
+        rotation = np.array([theta, 0, np.pi + phi])
+        rotation += np.random.randn(3) * self.opts['camera_noise']
+
+        self.camera.location = np.zeros(3)
+        self.camera.rotation_euler[:] = rotation
+        self.camera.location = location
+        return self.camera
+
+    def render(self, path, seq=0):
+        """Render the scene; should have seq < 999 to avoid non-canonical naming"""
+        if not os.path.exists(path):
+            os.makedirs(path)
+        bpy.data.scenes[0].render.filepath = os.path.join(path, "{:03d}.png".format(seq))
+        bpy.data.scenes[0].cycles.samples = self.opts['cycles_samples']
+
+        bpy.ops.render.render(write_still=True)
