26adf6872239cd614b71c3d10a20ca80253dde3c
Jaan Toots <jaantoots@gmail.com>
2016-07-23 23:40:11 +0100

    Rewrite set-blender

--- set-blender.sh
+++ set-blender.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
-PYTHONPATH=`python3 -c 'import sys; print(*sys.path, sep=":")'`
+BLENDER=$1
+[ -z "$BLENDER" ] && BLENDER=blender && [ -z $(which blender) ] \
+    && echo "error: blender not found: provide path to blender as an argument" && exit 1
 
-alias blender="env -v PYTHONPATH=\":$PYTHONPATH\" $1"
+PYTHONPATH=$(python3 -c 'import sys; print(*sys.path, sep=":")')
+
+if [ $(uname) == "Darwin" ]; then
+    # Verbose env for Mac OS X
+    alias blender="env -v PYTHONPATH=\":$PYTHONPATH\" $BLENDER"
+else
+    # Unfortunately Linux doesn't have this flag
+    alias blender="env PYTHONPATH=\":$PYTHONPATH\" $BLENDER"
+fi
