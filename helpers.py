810fcfa01b152a15f2244a53ec6217dfc98b25bc
Jaan Toots <jaantoots@gmail.com>
2016-08-09 15:13:41 +0100

    Improve style

--- helpers.py
+++ helpers.py
@@ -1,15 +1,17 @@
 """Objects used by multiple modules."""
 import json
 import numpy as np
-import bpy # pylint: disable=import-error
-import mathutils # pylint: disable=import-error
+import bpy  # pylint: disable=import-error
+import mathutils  # pylint: disable=import-error
+
 
 class Dict(dict):
     """Overload the missing method of builtin dict for brevity."""
 
     def __missing__(self, key):
-        """Return an empty list if missing element: can always append to dict[key]."""
+        """Return an empty list if missing element: append to dict[key]."""
         return []
+
 
 def all_instances(part: str, objects):
     """Return all objects with a given name or all if part is None."""
@@ -17,6 +19,7 @@ def all_instances(part: str, objects):
         return objects
     else:
         return [obj for obj in objects if part in obj.name.split('.')]
+
 
 def landscape_tree(landscape):
     """Return a balanced tree of landscape vertices for find operations."""
@@ -26,13 +29,15 @@ def landscape_tree(landscape):
     tree.balance()
     return tree
 
+
 def bounding_box(obj):
-    """Return a bounding box for a single object aligned with the global axes."""
+    """Return a bounding box for an object aligned with the global axes."""
     vertices = [obj.matrix_world * vertex.co for vertex in obj.data.vertices]
     box = np.zeros((2, 3))
     box[0] = np.min(vertices, axis=0)
     box[1] = np.max(vertices, axis=0)
     return box
+
 
 class BoundingSphere():
     """Sphere surrounding the objects.
@@ -42,7 +47,7 @@ class BoundingSphere():
     """
 
     def __init__(self, centre=None, radius=None):
-        """Initialise a bounding sphere with given centre and radius if given."""
+        """Initialise a bounding sphere with centre and radius (if given)."""
         self.centre = centre
         self.radius = radius
         self.vis = None
@@ -76,18 +81,22 @@ class BoundingSphere():
             self.vis = None
 
     def find(self, objects: list, centre=None):
-        """Return a bounding sphere for the objects with optionally specified centre."""
-        # TODO: Returns too large spheres and bounding box is not always correct.
+        """Return a bounding sphere for objects with optional centre."""
+        # TODO: Spheres are too large and bounding box is not always correct.
         def minmax(index, axis):
-            """Choose min or max depending on axis at bounding box corner index."""
-            is_max = (index >> axis) % 2 # Control bit in index corresponding to axis
+            """Choose min or max depending on axis at box corner index."""
+            is_max = (index >> axis) % 2  # Control bit in index for axis
             if axis == 0:
-                is_max ^= (index >> 1) % 2 # Cyclic index: 0 -> 00, 1 -> 01, 2 -> 11, 3 -> 10
+                # Cyclic index: 0 -> 00, 1 -> 01, 2 -> 11, 3 -> 10
+                is_max ^= (index >> 1) % 2
             return max if is_max else min
 
-        # For every corner i of bounding box, for axis j, choose min/max of all objects along axis
-        box = np.array([[minmax(i, j)([(x.matrix_world * mathutils.Vector(x.bound_box[i]))[j]
-                                       for x in objects]) for j in range(3)] for i in range(8)])
+        # For corner i of box, for axis j, choose min/max of objects along axis
+        box = np.array([
+            [minmax(i, j)(
+                [(x.matrix_world * mathutils.Vector(x.bound_box[i]))[j]
+                 for x in objects]
+            ) for j in range(3)] for i in range(8)])
         self.centre = np.sum(box, axis=0)/8 if centre is None else centre
         self.radius = np.max(np.linalg.norm(box - self.centre, axis=1))
         return self.centre, self.radius
